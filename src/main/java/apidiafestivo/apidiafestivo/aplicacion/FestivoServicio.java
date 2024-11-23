c09cdf56b073cdf8b30445a05f543293ad0e1387
lufemuag <kiritogamer13331@gmail.com>
2024-11-22 21:18:16 -0500

    seguridad

--- src/main/java/apidiafestivo/apidiafestivo/aplicacion/FestivoServicio.java
+++ src/main/java/apidiafestivo/apidiafestivo/aplicacion/FestivoServicio.java
@@ -18,7 +18,7 @@ public class FestivoServicio implements IFestivoServicio {
         this.repositorio = repositorio;
     }
 
-
+    //jejeje
     @Override
     public Date getDomingo(Integer año) {
         // Lógica para calcular Domingo de Ramos
@@ -37,7 +37,7 @@ public class FestivoServicio implements IFestivoServicio {
         return java.sql.Date.valueOf(java.time.LocalDate.of(año, mes, dia));  // Cambié a Date
     }
 
-    //jeje
+    
 
     @Override
     public Date getDomingoRamos(Integer año) {
